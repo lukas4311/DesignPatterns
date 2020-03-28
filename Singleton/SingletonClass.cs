5d4a59a34e6c628786f60fc5aeabc7893d75d642
lsalficky <lsalficky@NB-IT-SALFICKY>
2020-03-28 07:57:51 +0100

    Add basic singleton pattern #NotThreadSafe

---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    public class SingletonClass
+    {
+        private static SingletonClass instance;
+
+        private SingletonClass()
+        {}
+
+        public static SingletonClass GetInstance()
+        {
+            if (instance == null)
+                instance = new SingletonClass();
+
+            return instance;
+        }
+    }
+}
