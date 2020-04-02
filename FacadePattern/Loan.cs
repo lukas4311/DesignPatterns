3d0239842aa1fe71212deeff630e15049ee3b23b
lsalficky <lsalficky@NB-IT-SALFICKY>
2020-04-02 21:56:24 +0200

    Facade pattern added

---
+++ FacadePattern/Loan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FacadePattern
+{
+    internal class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+}
---
+++ FacadePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mortgage mortgage = new Mortgage();
+
+            Customer customer = new Customer("John Doe");
+            bool eligible = mortgage.IsEligible(customer, 125000);
+
+            Console.WriteLine("\n" + customer.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+
+            Console.ReadKey();
+        }
+    }
+}
