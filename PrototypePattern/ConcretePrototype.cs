31066cc71131a75fa043eac6f437684f99343f67
lsalficky <lsalficky@NB-IT-SALFICKY>
2020-04-02 21:21:50 +0200

    Prototype pattern

---
+++ PrototypePattern/ConcretePrototype.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    internal class ConcretePrototype : IPrototype
+    {
+        internal int Value { get; set; }
+
+        internal string Name { get; set; }
+
+        public IPrototype DoDeepCopy()
+        {
+            ConcretePrototype clone = this.MemberwiseClone() as ConcretePrototype;
+            clone.Name = new string(Name);
+            return clone;
+        }
+
+        public IPrototype DoShallowCopy()
+        {
+            return this.MemberwiseClone() as ConcretePrototype;
+        }
+    }
+}
