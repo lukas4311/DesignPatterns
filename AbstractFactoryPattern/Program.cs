f3d8d8946b1e3d197df3daeb264ef15eda69617c
lsalficky <lsalficky@NB-IT-SALFICKY>
2020-04-02 16:21:00 +0200

    AbstractFactory - unknown type of car protection

--- AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AbstractFactoryPattern
 {
@@ -6,9 +8,18 @@ namespace AbstractFactoryPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("**** Zadejte typ vozidla ****");
             Client client = new Client();
-            string carName = Console.ReadLine();
+            string carName = string.Empty;
+
+            do
+            {
+                if(!string.IsNullOrEmpty(carName))
+                    Console.WriteLine("**** Zadaný typ vozidla je neznámý ****");
+
+                Console.WriteLine("**** Zadejte typ vozidla ****");
+                carName = Console.ReadLine();
+            } while (!client.IsKnownCreateProcessOfCarType(carName));
+
             client.CreateCar(carName);
             Console.ReadLine();
         }
@@ -18,14 +29,19 @@ namespace AbstractFactoryPattern
     {
         private ICarFactory vehicleFactory = null;
 
+        public Dictionary<string, Func<ICarFactory>> FactoryDictionaryOfFactoryTypes { get; set; } = new Dictionary<string, Func<ICarFactory>>
+        {
+            { "toyota", () => new ToyotaFactory()},
+            { "bmw", () => new BMWFactory()}
+        };
+
+        public bool IsKnownCreateProcessOfCarType(string carType)
+        {
+            return this.FactoryDictionaryOfFactoryTypes.Keys.Count(k => string.Equals(k, carType, StringComparison.OrdinalIgnoreCase)) != 0;
+        }
+
         public void CreateCar(string carName)
         {
-            if (string.IsNullOrEmpty(carName))
-            {
-                Console.WriteLine("Název typu vozidla je prázdný");
-                return;
-            }
-
             if (string.Equals(carName, "toyota", StringComparison.OrdinalIgnoreCase))
             {
                 vehicleFactory = new ToyotaFactory();
