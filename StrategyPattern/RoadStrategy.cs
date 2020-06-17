0d4b1d96e898f1c4bd838f461f55e0b91b520d6a
lukas4311 <lukas4311@gmail.com>
2020-06-17 21:43:38 +0200

    Added new strategies

---
+++ StrategyPattern/RoadStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    public class RoadStrategy
+    {
+        public GeoCoordinate[] FindShortestPath(GeoCoordinate source, GeoCoordinate target)
+        {
+            // find shorted path for walk
+            Console.WriteLine("By car to target");
+            return new[] { new GeoCoordinate(48.145, 48.2546) };
+        }
+    }
+}
---
+++ StrategyPattern/WalkingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    public class WalkingStrategy : IStrategy
+    {
+        public GeoCoordinate[] FindShortestPath(GeoCoordinate source, GeoCoordinate target)
+        {
+            // find shorted path for walk
+            Console.WriteLine("Walking to target");
+            return new[] { new GeoCoordinate(49.055, 50.165) };
+        }
+    }
+}
